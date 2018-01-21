1a060aa7d1f8400b28c83d1133651a41b2c5488b
Ryan Roat <ryanroat@gmail.com>
2018-01-21 01:18:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,8 +16,8 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    
-  })
+    alert("Your form is going to be submitted now.");
+  });
 }
 
 function frameIt() {
